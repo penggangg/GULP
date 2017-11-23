e9033ba681c8ec48ec5ae610b5d932584f5c96bd
unknown <918800642@qq.com>
2017-11-23 15:22:44 +0800

    view

---
+++ view/static/js/demo2.js
@@ -0,0 +1,1 @@
+var box1=document.getElementsByClassName("box1")[0];box1.onclick=function(){console.log(456)};
---
+++ view/static/jsAll/demo2.js
@@ -0,0 +1,4 @@
+var box1=document.getElementsByClassName('box1')[0];
+box1.onclick=function(){
+    console.log(456)
+}
